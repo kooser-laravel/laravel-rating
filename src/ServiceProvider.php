f7477277c438acb21a3c231fe0c9998d04ff422b
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-04-18 16:53:49 -0400

    Add service provider.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kooser\Laravel\Rating;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+/**
+ * The service provider.
+ */
+class ServiceProvider extends LaravelServiceProvider
+{
+}
